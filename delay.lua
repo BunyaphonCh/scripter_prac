69657151c91269b69bbbabeabe027238fba59f83
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-04-14 18:40:36 +0700

    chore: add remind about coroutine

--- delay.lua
+++ delay.lua
@@ -3,6 +3,7 @@
 -- wait มันก็เหมือน delay ต่างที่ wait มันจะรอที่ fetch หลัก
 -- เเต่ delay จะไม่หยุด fetch หลัก หรือก็คือ while true ที่อยู่ด้านหลัง
 -- parameter เเรกของ delay ืคือ จำนวนวินาทีว่ารอกี่วิ ค่อยสร้าง fetch ขึ้นมา ส่วน parameter ที่สองก็จะเป็น function
+-- ว่างๆจะไปลองดูเรื่อง coroutine
 
 -- มันจะ warn World ไปเรื่อยๆ พอผ่านไป 5 วิ ก็จะกลับมา warn Hello ด้วย
 delay(5, function()
