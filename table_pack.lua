45235f55b14a8fed6203ce817eefa73ff40dc9c3
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-04-15 23:10:11 +0700

    test: using table.pack for table creation and it auto-generated n key

---
+++ table_pack.lua
@@ -0,0 +1,16 @@
+-- table จะมี key กะ value
+local Bell = {
+	age = 18,
+	nice = true,
+	smart = false,
+	food = "ramen",
+	pokemon = "yamper"
+}
+
+--local Animal = {"elephant", "chicken", "dog", "cat", "fish"}
+
+-- วิธีสร้าง table อีกเเบบนึง
+-- ข้อเสียคือมันจะมี key n ด้วย เป็นการบอกจำนวน index
+local Animal = table.pack("elephant", "chicken", "dog", "cat", "fish")
+
+warn(Animal)
