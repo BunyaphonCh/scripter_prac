728252fdb2e4aa682a5f4dad6db9f99f78f00ead
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-04-15 17:43:57 +0700

    test: verify table.clear output in console

---
+++ table_clear.lua
@@ -0,0 +1,15 @@
+-- table จะมี key กะ value
+local Bell = {
+	age = 18,
+	nice = true,
+	smart = false,
+	food = "ramen",
+	pokemon = "yamper"
+}
+
+local Animal = {"elephant", "chicken", "dog", "cat", "fish"}
+
+-- set ให้ทุกอย่างใน table กลายเป็นค่า nil
+table.clear(Animal)
+
+warn(Animal) -- มันก็จะ warn ออกมาเป็นค่าว่าง หรือก็คือ {}
